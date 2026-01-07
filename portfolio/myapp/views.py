93ade3c9050dc66e12740ef8e4d97458d117179e
sanjaysaini16122000-ui <sanjaysaini16122000@gmail.com>
2026-01-07 18:03:22 +0530

    Contact form backend with database save

--- portfolio/myapp/views.py
+++ portfolio/myapp/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import ContactMessage
 
 def home(request):
     return render(request, "home.html")
@@ -10,4 +12,24 @@ def projects(request):
     return render(request, "projects.html")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        data = {
+            'name': name,
+            'email': email,
+            'message': message
+        }
+        ContactMessage.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+        print(data)
+
+        return HttpResponse("Thank you! Your message has been sent.")
+
     return render(request, "contact.html")
